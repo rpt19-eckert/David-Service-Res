1303e7c1f3dcef169e678d89ed5424c0d8b6193c
davidlira1 <liradavid100@gmail.com>
2020-05-04 16:19:31 -0700

    load_to_pgSQL can now add the db with the 2 tables. works good

--- database/simulated-dataset/load-data/load_to_pgSQL.js
+++ database/simulated-dataset/load-data/load_to_pgSQL.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const client = new Client({
+var client = new Client({
     user: 'postgres',
     password: 'pw'
 });
@@ -8,6 +8,51 @@ client.connect();
 
 
 client
-.query('SELECT NOW() as now')
-.then(res => console.log(res.rows[0]))
+.query('DROP DATABASE IF EXISTS reservation_service')
+.then(() => {
+    return client.query('CREATE DATABASE reservation_service');
+})
+.then(() => {
+    console.log('Created database reservation_service');
+    client.end();
+    client = new Client({
+        user: 'postgres',
+        password: 'pw',
+        database: 'reservation_service'
+    })
+    client.connect();
+
+    return client.query(
+        `CREATE TABLE listings (
+            listingId SERIAL NOT NULL PRIMARY KEY,
+            listingName VARCHAR(100),
+            pricePerNight DECIMAL(5, 2) NOT NULL,
+            weekend integer NOT NULL default 0,
+            weekendPrice DECIMAL(3, 2) NOT NULL,
+            maxGuests int NOT NULL,
+            tax DECIMAL(3, 2) NOT NULL
+        )`
+    );
+
+})
+.then(() => {
+    console.log('Created table listings');
+    return client.query(
+        `CREATE TABLE bookings (
+            id SERIAL NOT NULL PRIMARY KEY,
+            listingId int,
+            nights int,
+            month VARCHAR(4),
+            checkIN VARCHAR(10),
+            checkOut VARCHAR(10),
+            guests int,
+            children int default 0,
+            infants int default 0
+        )`
+    );
+})
+.then(() => {
+    console.log('Created table bookings');
+    client.end();
+})
 .catch(e => console.error(e.stack));
