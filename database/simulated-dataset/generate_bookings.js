da86dedf5f954d0d282c75faa2679dd06b3d55a1
davidlira1 <liradavid100@gmail.com>
2020-05-03 22:28:40 -0700

    complete generate_bookings.js. now moving to write_data_to_files.js

--- database/simulated-dataset/generate_bookings.js
+++ database/simulated-dataset/generate_bookings.js
@@ -5,77 +5,59 @@ var randomNum = (min, max) => {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-var makeCalendarDays = () => {
-    var calendarDays = [];
-    _each(monthsDays, (monthDays) => {
-        for(var i = 1; i <= monthDays; i++) {
-            calendarDays.push(i);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var dateBasedOnCalendarIdx = (acum) => {
+    var accumulator = 0;
+    var month = 0;
+    var day = 0;
+    _.each(monthsDays, (monthDays, idx) => {
+        accumulator+= monthDays;
+        month = idx + 1;
+        if (acum <= accumulator) {
+            month = month < 10 ? `0${month}` : `${month}`;
+            day = acum - (accumulator - monthDays);
+            return false;
         }
     })
-    return calendarDays;
+    return `${month}-${day}`;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var checkWhichMonth = function (acum) {
-
-
-//   var month;
-//   if (acum < 30) {
-//    month = '04'; //April
-//   } else if (acum >= 30 && acum <= 60) {
-//   month = '05'; //May
-//   } else if (acum >= 61 && acum <= 90 ) {
-//     month = '06'; //June
-//   } else if (acum >= 91 && acum <= 121 ) {
-//     month = '07';  //July
-//   }
-//   return month;
-}
-
-var generateBookingsPerListing = (id, listingId) => {
-    var bookingsDataForListing = '';
+var generateBookingsForListing = (id, listingId, numOfBookings) => {
+    var bookingData = '';
     var acum = 0;
 
-    var nights = randomNum(2, 5);
+    for (var i = 0; i < numOfBookings; i++) {
+        var nights = randomNum(2, 5);
     
-    var rangeInBetween = randomNum(4, 12);
-    
-    var guests = randomNum(1, 2);
+        var rangeInBetween = randomNum(9, 16);
+        var start = rangeInBetween + acum;
+        var end = nights + start
+        var checkInDate = dateBasedOnCalendarIdx(start);
+        var checkOutDate =  dateBasedOnCalendarIdx(end);
+        
+        var guests = randomNum(1, 2);
+        var children = randomNum(0, 1);
+        var infants = randomNum(0, 1);
+        
+        acum = end;
 
-    var startingPoint = rangeInBetween + acum;
-
-    var start = arr[startingPoint];
-    
-    var end = arr[nights + startingPoint];
-   
-
-    var startMonth = checkWhichMonth(startingPoint);
-    var checkInDate = `${startMonth}-${start}`;
-
-    var endMonth = checkWhichMonth(days + startingPoint);
-    var checkOutDate =  `${endMonth}-${end}`;
-
-    acum = nights + startingPoint;
-
-    var startMonthSliced = startMonth.slice(0, 2);
-    
-    bookingsDataForListing += `${id},${listingId},${nights},"${startMonthSliced}","${checkInDate}","${checkOutDate}",${guests},0,0\r\n`;
-
-    return bookingsDataForListing;
-}
-
-var generateBookings = () => {
-    var calendarDays = makeCalendarDays();
-    var bookingsData = '';
-    var id = 0;
-    for (var i = 1; i <= 100; i++) {
-        for (var k = 0; k < randomNum(0, 10); k++) {
-            id++;
-            bookingsData += generateBookingsPerListing(id, i + 10000);
-        }
+        var startMonthSliced = checkInDate.slice(0, 2);
+        
+        bookingData += `${id + i},${listingId},${nights},"${startMonthSliced}","${checkInDate}","${checkOutDate}",${guests},${children},${infants}\r\n`;
     }
-
+    return bookingData;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var generateBookings = (numOfListings) => {
+    var bookingsData = '';
+    var id = 1;
+    var numOfBookings = 0;
+    for (var i = 1; i <= numOfListings; i++) {
+        numOfBookings = randomNum(2, 12);
+        bookingsData += generateBookingsForListing(id, i + 10000, numOfBookings);
+        id+= numOfBookings;
+    }
     return bookingsData;
 };
 
-
+module.exports = generateBookings;
--- database/simulated-dataset/write_data_to_files.js
+++ database/simulated-dataset/write_data_to_files.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const generateListings = require('./generate_listings');
+const generateBookings = require('./generate_bookings');
 const listings = generateListings();
 
 fs.writeFileSync(__dirname + '/listings.txt', listings);
